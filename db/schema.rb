53822efe8ea866e1969b621ebb0ec75c0014ef5f
KazuyaSaitoMirai <kazuya.saito.mirai@gmail.com>
2017-01-21 21:09:28 +0900

    bundle updating sqlite3

--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,13 @@
 
 ActiveRecord::Schema.define(version: 20170118180421) do
 
-  create_table "listings", force: true do |t|
-    t.string   "home_type"
-    t.string   "pet_type"
-    t.string   "pet_size"
+  create_table "listings", force: :cascade do |t|
+    t.string   "home_type",       limit: 255
+    t.string   "pet_type",        limit: 255
+    t.string   "pet_size",        limit: 255
     t.integer  "breeding_years"
-    t.string   "address"
-    t.string   "listing_title"
+    t.string   "address",         limit: 255
+    t.string   "listing_title",   limit: 255
     t.text     "listing_content"
     t.integer  "price_pernight"
     t.boolean  "active"
@@ -30,37 +30,37 @@ ActiveRecord::Schema.define(version: 20170118180421) do
 
   add_index "listings", ["user_id"], name: "index_listings_on_user_id"
 
-  create_table "photos", force: true do |t|
+  create_table "photos", force: :cascade do |t|
     t.integer  "listing_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
+    t.string   "image_file_name",    limit: 255
+    t.string   "image_content_type", limit: 255
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
   end
 
   add_index "photos", ["listing_id"], name: "index_photos_on_listing_id"
 
-  create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "image"
-    t.string   "name"
-    t.string   "phone_number"
-    t.string   "description"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.string   "provider",               limit: 255
+    t.string   "uid",                    limit: 255
+    t.string   "image",                  limit: 255
+    t.string   "name",                   limit: 255
+    t.string   "phone_number",           limit: 255
+    t.string   "description",            limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
